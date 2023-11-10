7b700d729c28276788f8f998a7ff162aa2daf5e4
RogueDXZ <52176973+RogueDXZ@users.noreply.github.com>
2023-11-10 15:44:50 -0600

    Update Musica.sh

--- Scripts/Musica.sh
+++ Scripts/Musica.sh
@@ -1,1 +1,29 @@
+# Imprimir sobre el programa corriendo
+echo "=========================="
+echo "Musica automatica iniciada"
+echo "=========================="
 
+# Directorio de msica a reproducir
+MUSIC_DIR="~/storage/music"
+
+# PID del proceso de mpv
+MPV_PID=""
+
+# Bucle infinito para verificar la conexin Bluetooth
+while true; do
+    # Verificar si se han conectado audfonos Bluetooth
+    if termux-audio-info | grep -q '"BLUETOOTH_A2DP_IS_ON": true'; then
+        # Si los audfonos Bluetooth estn conectados y mpv no est reproduciendo msi>
+        if [ -z "$MPV_PID" ]; then
+            sleep 3
+            mpv --no-video --shuffle --loop-playlist ~/storage/music/*.mp3 &
+            MPV_PID=$!
+        fi
+    else
+        # Si los audfonos Bluetooth estn desconectados y mpv est reproduciendo msi>
+        if [ -n "$MPV_PID" ]; then
+            pkill mpv
+            MPV_PID=""
+        fi
+    fi
+done
