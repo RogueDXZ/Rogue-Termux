180cb53c88ac722fd5481a70cf04d832d6432f04
RogueDXZ <52176973+RogueDXZ@users.noreply.github.com>
2023-11-10 16:01:05 -0600

    Create QuickSetup.sh

---
+++ QuickSetup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Clonar Repositorio
+git clone 
+
+#Instalar desde repositorios de PKG
+pkg update
+pkg install htop neofetch termux-api openssh speedtest-go nmap ani-cli termimage -y
+
+#Instalar desde repositorios de APT
+apt update -y
+apt upgrade -y
+apt install python pip mpv w3m cmatrix tmate tmux sox
+
+
+#Eliminar archivo de Setup
+rm QuickSetup.sh
