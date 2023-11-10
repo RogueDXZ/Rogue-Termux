20ec03e0a9f5a732d4b1c0b9ae409fee005e8425
RogueDXZ <52176973+RogueDXZ@users.noreply.github.com>
2023-11-10 16:19:19 -0600

    Update QuickSetup.sh

--- QuickSetup.sh
+++ QuickSetup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#Clonar Repositorio
-#git clone https://github.com/RogueDXZ/Rogue-Termux/
-
 #Instalar desde repositorios de PKG
 pkg update
 pkg install htop neofetch termux-api openssh speedtest-go nmap ani-cli termimage -y
@@ -12,6 +9,21 @@ apt update -y
 apt upgrade -y
 apt install python pip mpv w3m cmatrix tmate tmux sox
 
+#Reposicionar en $HOME
+cd $HOME
+
+#Clonar Repositorio
+git clone https://github.com/RogueDXZ/Rogue-Termux/
+
+#Mover elementos del git clone a el directorio $HOME
+mv Rogue-Termux/* $HOME
+
+#Hacer ejecutables todos los Scripts
+chmod +x $HOME/Scripts/*.sh
+chmod +x $HOME/Scripts/.*.sh
+
+#Hacer un "source" a .profile
+source $HOME/.profile
 
 #Eliminar archivo de Setup
 rm QuickSetup.sh
