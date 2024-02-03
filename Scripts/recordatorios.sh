da4f95e63ae59c5f533b1f6dd7d843877f839722
RogueDXZ <rodolforosas9927@gmail.com>
2024-02-03 12:21:56 -0600

    Cambios

--- Scripts/recordatorios.sh
+++ Scripts/recordatorios.sh
@@ -46,9 +46,12 @@ chmod +x ~/Scripts/cosasquehacer.sh
 ;;
 
 3)
-
+if cat ~/Scripts/cosasquehacer.sh | grep -q "Cancelar"; then
 linea=$(cat -n ~/Scripts/cosasquehacer.sh | fzf --reverse -e -i --prompt="¿Qué recordatorio quieres eliminar?: " | awk '{print $1}') && sed -i "${linea}d" ~/Scripts/cosasquehacer.sh
-
+else
+echo "Cancelar" >> ~/Scripts/cosasquehacer.sh
+linea=$(cat -n ~/Scripts/cosasquehacer.sh | fzf --reverse -e -i --prompt="¿Qué recordatorio quieres eliminar?: " | awk '{print $1}') && sed -i "${linea}d" ~/Scripts/cosasquehacer.sh
+fi
 ;;
 
 4)
